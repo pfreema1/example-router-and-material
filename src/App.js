63085f70598a0444918eef8e89bd12e666cd931e
Paul <pfreema1@gmail.com>
2018-04-13 21:06:08 -0400

    foo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import FooContainer from "./Containers/FooContainer";
 import BarContainer from "./Containers/BarContainer";
 import RaisedButton from "material-ui/RaisedButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { Provider } from "react-redux";
+import createHistory from "history/createBrowserHistory";
+import {
+  ConnectedRouter,
+  routerReducer,
+  routerMiddleware,
+  push
+} from "react-router-redux";
 import "./App.css";
+import CustomContainer from "./Containers/CustomContainer";
+
+const initialState = {};
+const history = createHistory();
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const data = {
+  buttonTextArr: ["foo", "mosdafsso", "baz"],
+  cardTitle: "sdlfjsadljfs",
+  cardSubtitle: "card subtitle"
+};
+
+const store = createStore(reducer);
 
 const Home = () => (
   <div className="options-wrapper">
@@ -27,15 +55,17 @@ const Home = () => (
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <Router>
-          <div>
-            <Route exact path="/" component={Home} />
-            <Route path="/showFoo" component={FooContainer} />
-            <Route path="/showBar" component={BarContainer} />
-          </div>
-        </Router>
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <ConnectedRouter history={history}>
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route path="/showFoo" component={FooContainer} />
+              <Route path="/showBar" component={() => <CustomContainer />} />
+            </div>
+          </ConnectedRouter>
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
